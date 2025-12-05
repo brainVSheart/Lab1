5a8db3cd1e3194a0777a66523254b79ce6eb36a7
_0n0_/idontno <5178gt@gmail.com>
2025-12-05 15:55:04 +0700

    Update lab.cpp

--- Lab1.1/lab.cpp
+++ Lab1.1/lab.cpp
@@ -1,22 +1,30 @@
 #include <stdio.h>
-int main() {
-    int N, i;
-  while (1){
+int main()
+{
+  int N, i;
+  while (1)
+  {
     printf("Enter rounds:");
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1)
+    {
       printf("Error\n");
-        return 1;
+      return 1;
     }
-    if(N==0){
+    if (N == 0)
+    {
       break;
     }
-      if (N>0){
-        for(i=0;i<N;i++){
+    if (N > 0)
+    {
+      for (i = 0; i < N; i++)
+      {
         printf("Hello Loop\n");
-        }
-      }else{
-        printf("Error\n");
       }
+    }
+    else
+    {
+      printf("Error\n");
+    }
   }
-   return 0;
+  return 0;
 }
