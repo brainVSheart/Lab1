a9707ba899042fa0d8e3b95ff534b6d125666aaf
_0n0_/idontno <5178gt@gmail.com>
2025-12-05 16:27:15 +0700

    Add files via upload

---
+++ Lab1.1/lab.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int N, i;
+  while (1)
+  {
+    printf("Enter rounds:");
+    if (scanf("%d", &N) != 1)
+    {
+      printf("Error\n");
+        return 1;
+    }
+    if(N==0)
+    {
+      break;
+    }
+      if (N>0)
+      {
+        for(i=1;i<=N;i++)
+        {
+        printf("[%d] Hollo loop\n",i);
+        }
+      }else{
+        printf("Error\n");
+      }
+  }
+   return 0;
+}
